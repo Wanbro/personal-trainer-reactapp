3890f46fe84e110ad71f8d4ff339a23d1684c6fd
Aleksanteri Fagerholm <48619709+Wanbro@users.noreply.github.com>
2019-11-22 17:44:56 +0200

    Added edit customer functionality
    added edit customer functionality, now you can edit customer based on the customers id

--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -3,6 +3,7 @@ import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
+import Editcustomer from './Editcustomer';
 import Addcustomer from './Addcustomer';
 import Grid from '@material-ui/core/Grid';
 
@@ -42,6 +43,20 @@ const Customerlist = () => {
             .catch(err => console.error(err))
     }
 
+    const updateCustomer = (customer, link) => {
+        fetch(link, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(customer)
+        })
+            .then(res => fetchCustomers())
+            .then(setMessage('Changes saved succesfully'))
+            .then(res => setOpen(true))
+            .catch(err => console.error(err))
+    }
+
     const deleteCustomer = (link) => {
         if (window.confirm('Are you sure?')) {
         fetch(link, {method: 'DELETE'})
@@ -51,8 +66,15 @@ const Customerlist = () => {
         .catch(err => console.error(err))
        } }
 
-    const Customercolumns = [
-    {    
+    const Customercolumns = [{
+        filterable: false,
+        sortable: false,
+        width: 100,
+        Cell: row => <Editcustomer updateCustomer={updateCustomer} customer={row.original} />
+
+    },
+         
+        {    
     
         accessor: 'links[0].href',
         filterable: false,
