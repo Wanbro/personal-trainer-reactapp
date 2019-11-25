91fce167b395621bcd30f44f4e8ee779f7d1ab99
Aleksanteri Fagerholm <48619709+Wanbro@users.noreply.github.com>
2019-11-25 23:34:34 +0200

    linking user to a certain training

--- src/components/Traininglist.js
+++ src/components/Traininglist.js
@@ -3,11 +3,14 @@ import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 import Snackbar from '@material-ui/core/Snackbar';
 import Button from '@material-ui/core/Button';
+import AddTraining from './Addtraining'
 import moment from 'moment';
+import Grid from '@material-ui/core/Grid';
 
 
 const Traininglist = () => {
     const [training, setTraining] = useState([]);
+   
 
    useEffect(() => {
     fetchTraining();
@@ -21,16 +24,38 @@ const Traininglist = () => {
     })
    };
 
+   const saveTraining = (newTraining) => {
+    fetch('https://customerrest.herokuapp.com/api/trainings', {
+    method: 'POST', 
+    headers: {'Content-type': 'application/json'}, 
+    body: JSON.stringify(newTraining)
+   })
+    .then(res => fetchTraining())
+    .catch(err => console.error(err))
+}
+
    const columns = [{
     id: 'date',
     Header: 'date',
         accessor: d => {
         return moment(d.date)
-          .format("hh:mm DD.MM.YYYY")
+          .format("DD.MM.YYYY")
       },
+      
+      
 
 
-    }, {
+    },
+    {
+        id: 'time',
+        Header: 'start time',
+            accessor: t => {
+            return moment(t.date)
+              .format("hh:mm")
+          }
+        },
+    
+    {
     Header: 'duration',
     accessor: 'duration',
     },
@@ -40,14 +65,20 @@ const Traininglist = () => {
     Header: 'activity',
     accessor: 'activity',
       
+    }, {
+
+        id: 'customer', // Required because our accessor is not just a string
+        Header: 'customer',
+        accessor: d => d.customer.firstname + " " + d.customer.lastname,
+
     }]
 
    return (
     <div>
-        <grid container>
-    
-    
-   </grid>
+        <Grid container>
+        <AddTraining saveTraining = {saveTraining} fetchTraining = {fetchTraining}/>
+        
+       </Grid>
    
     <ReactTable filterable={true} columns={columns} data={training}/>
      </div>
